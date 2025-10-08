391215746d29a99d280d112e9b750ad4d1fa005a
top13 <top13@DESKTOP-ER8N1NS>
2025-10-08 16:58:50 +0300

    Добавьте файлы проекта.

---
+++ Калькулятор/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Калькулятор
+{
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+    public class AssemblyInfo
+    {
+        [assembly: AssemblyTitle("Калькулятор")]
+        [assembly: AssemblyDescription("Мой первый калькулятор")]
+        [assembly: AssemblyCompany("TUZMAN TEAM")]
+        [assembly: AssemblyProduct("KalTuz")]
+        [assembly: AssemblyCopyright("Copyright © TUZMAN 2024")]
+        [assembly: AssemblyTrademark("KalTuz")]
+        [assembly: AssemblyVersion("1.0.0.0")]
+        [assembly: AssemblyFileVersion("1.0.0.0")]
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+}
---
+++ Калькулятор/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Калькулятор
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Напишите первое число:");
+                double one = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Напишите действие *,/,+,-,%");
+                string operation = Console.ReadLine();
+                Console.WriteLine("Напишите второе число:");
+                double two = Convert.ToDouble(Console.ReadLine());
+
+                if (operation == "*")
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.Write(one * two);
+                }
+                else if (operation == "+")
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.Write(one + two);
+                }
+                else if (operation == "-")
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.Write(one - two);
+                }
+                else if (operation == "/")
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.Write(one / two);
+                }
+                else if (operation == "%") 
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.Write(one / 100 * two);
+                }
+                
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
